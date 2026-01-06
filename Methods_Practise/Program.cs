1d62a4878949bba4d0d858cbbaefa459945abf5d
Naidu-toruk <sadinenibrahmanaidu@gmail.com>
2026-01-06 11:15:08 +0530

    Add project files.

---
+++ Methods_Practise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Practise
+{
+    internal class Program
+    {
+        public String SampleMethod()
+        {
+            return "This is a sample method.";
+        }
+
+        static void Main(String[] args)
+        {
+            Program s = new Program();
+            Console.WriteLine(s.SampleMethod());
+        }
+    }
+}
